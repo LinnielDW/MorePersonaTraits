79f7f88311b9e8e81d141d2c02e2e1be3342f19d
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-07-20 15:54:16 +0200

    Added framework for onhit worker traits

---
+++ Source/MorePersonaTraitsWeaponTraitExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Verse;
+
+namespace MorePersonaTraits
+{
+    public class MorePersonaTraitsWeaponTraitExtension : DefModExtension
+    {
+        public Type OnHitWorkerClass = typeof(OnHitWorker);
+        private OnHitWorker _onHitWorker;
+        
+        public OnHitWorker OnHitWorker
+        {
+            get
+            {
+                if (_onHitWorker == null)
+                {
+                    _onHitWorker = (OnHitWorker) Activator.CreateInstance(OnHitWorkerClass);
+                    _onHitWorker.def = this;
+                }
+
+                return _onHitWorker;
+            }
+        }
+    }
+}
---
+++ Source/OnHitWorker.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+
+namespace MorePersonaTraits
+{
+    public class OnHitWorker
+    {
+        public float ProcChance = 0f;
+        public float DefaultMagnitude = 0f;
+        public MorePersonaTraitsWeaponTraitExtension def;
+        
+        public virtual void OnHitEffect() {
+        }
+
+    }
+}
