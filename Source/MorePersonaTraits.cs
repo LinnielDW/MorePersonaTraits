84f3fb4e01c549e89ac424cb825cb4c147aad202
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-07-20 10:35:57 +0200

    Added MPT_BondedResearchUp and changed MPT_ResearchUp to MPT_BondedResearchUp

--- Source/MorePersonaTraits.cs
+++ Source/MorePersonaTraits.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using Verse;
 
 namespace MorePersonaTraits
 {
-    public class MorePersonaTraits
+    public class MorePersonaTraits : Mod
     {
+        public MorePersonaTraits(ModContentPack content) : base(content)
+        {
+
+        }
     }
 }
