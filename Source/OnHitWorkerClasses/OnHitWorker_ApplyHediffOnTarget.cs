677501418317aa775090464afa53e3115f1b9f97
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-07-21 19:43:12 +0200

    Implemented worker for applying onhit effects to target

---
+++ Source/OnHitWorkerClasses/OnHitWorker_ApplyHediffOnTarget.cs
@@ -0,0 +1,26 @@
+﻿using Verse;
+
+namespace MorePersonaTraits.OnHitWorkerClasses
+{
+    public class OnHitWorker_ApplyHediffOnTarget : OnHitWorker
+    {
+        public override void OnHitEffect(Thing hitThing, Thing originThing)
+        {
+            if (hitThing != null && hitThing is Pawn hitPawn)
+            {
+                var hediffOnPawn = hitPawn.health?.hediffSet?.GetFirstHediffOfDef(WeaponTraitOnHitExtension.TargetThingHediff);
+
+                if (hediffOnPawn != null)
+                {
+                    hediffOnPawn.Severity += WeaponTraitOnHitExtension.ProcMagnitude;
+                }
+                else
+                {
+                    var hediff = HediffMaker.MakeHediff(WeaponTraitOnHitExtension.TargetThingHediff, hitPawn);
+                    hediff.Severity = WeaponTraitOnHitExtension.ProcMagnitude;
+                    hitPawn.health.AddHediff(hediff);
+                }
+            }
+        }
+    }
+}
