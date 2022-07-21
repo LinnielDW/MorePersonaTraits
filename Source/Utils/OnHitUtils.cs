9d3df3ec5d6ab4afbbee2938aad9f3a509b04fc1
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-07-21 19:39:20 +0200

    implemented rolling onhit proc chance

--- Source/Utils/OnHitUtils.cs
+++ Source/Utils/OnHitUtils.cs
@@ -9,10 +9,18 @@ namespace MorePersonaTraits.Utils
         public static void applyOnHitEffects(List<WeaponTraitOnHitExtension> onHitExtensions,
             Thing targetThing, Thing originThing)
         {
+            float rand = Rand.Value;
             foreach (var extension in onHitExtensions)
             {
-                //TODO: compare rand to proc chance.
-                extension.OnHitWorker.OnHitEffect(targetThing, originThing);
+                if (rand <= extension.ProcChance)
+                {
+                    // Log.Warning("Onhit has proc'ed. Executing OnHit effect");
+                    extension.OnHitWorker.OnHitEffect(targetThing, originThing);
+                }
+                else
+                {
+                    // Log.Warning("Onhit proc missed. Skipping onhit effect.");
+                }
             }
         }
     }
