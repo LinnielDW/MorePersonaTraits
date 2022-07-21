546ccf86eb3206b9cad95d9d7c8261f26075b5a1
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-07-21 19:38:21 +0200

    removed (currently) unused patch for bullet on-hit effects.

--- Source/Patches/PatchOnDamage.cs
+++ Source/Patches/PatchOnDamage.cs
@@ -5,26 +5,6 @@ using Verse;
 
 namespace MorePersonaTraits.Patches
 {
-    [HarmonyPatch(typeof(Bullet))]
-    [HarmonyPatch("Impact")]
-    public static class PatchOnDamageBullet
-    {
-        static void Postfix(Thing hitThing, Bullet __instance)
-        {
-            //TODO: check if this works on proj weapons
-
-            var primary = ((Pawn) __instance.Launcher).equipment.Primary;
-            if (PatcherCheckUtils.hasOnHitWorker(primary))
-            {
-                OnHitUtils.applyOnHitEffects(
-                    PatcherCheckUtils.getOnHitExtensions(__instance),
-                    hitThing,
-                    __instance.Launcher
-                );
-            }
-        }
-    }
-
     [HarmonyPatch(typeof(Verb_MeleeAttackDamage))]
     [HarmonyPatch("ApplyMeleeDamageToTarget")]
     public static class PatchOnDamageMelee
