5c01190de48c1f50dc32f5cb9c35cd649a8190dd
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-07-27 13:15:24 +0200

    added skeleton for WeaponTraitWorker_NeedFilledOnKill

--- Source/Templates/OnHitWorker_Template.cs
+++ Source/Templates/OnHitWorker_Template.cs
@@ -13,7 +13,7 @@ namespace MorePersonaTraits.Templates
 
         private void DebugApply(Pawn thing)
         {
-            Log.Warning("OnHitWorker_Template on-hit effect has been activated by " + thing.Name);
+            Log.Warning("OnHitWorker_Template on-hit effect has been activated on target: " + thing.Name);
         }
     }
 }
