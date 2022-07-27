61258b539806224bd7aee3759d26d2b2ffac9ad3
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-07-27 10:12:37 +0200

    added default constructor for WeaponTraitOnHitExtension

--- Source/Extensions/WeaponTraitOnHitExtension.cs
+++ Source/Extensions/WeaponTraitOnHitExtension.cs
@@ -8,6 +8,11 @@ namespace MorePersonaTraits.Extensions
 {
     public class WeaponTraitOnHitExtension : DefModExtension
     {
+        public static WeaponTraitOnHitExtension CreateInstance()
+        {
+            return new WeaponTraitOnHitExtension();
+        }
+
         public List<OnHitWorker> OnHitWorkers = null;
     }
 }
