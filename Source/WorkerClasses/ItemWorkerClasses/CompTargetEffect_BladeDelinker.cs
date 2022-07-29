71340ce7273e1a621d4485f22893ba692df02d6c
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-07-29 20:52:33 +0200

    added MPT_BladelinkUncoder

---
+++ Source/WorkerClasses/ItemWorkerClasses/CompTargetEffect_BladeDelinker.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MorePersonaTraits.WorkerClasses.ItemWorkerClasses
+{
+    public class CompTargetEffect_BladeDelinker : CompTargetEffect
+    {
+        public override void DoEffectOn(Pawn user, Thing target)
+        {
+            var compBladelink = target.TryGetComp<CompBladelinkWeapon>();
+            if (compBladelink == null)
+            {
+                return;
+            }
+
+            target.TryGetComp<CompBladelinkWeapon>().UnCode();
+        }
+    }
+}
---
+++ Source/WorkerClasses/ItemWorkerClasses/CompTargetable_SingleBladelink.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace MorePersonaTraits.WorkerClasses.ItemWorkerClasses
+{
+    public class CompTargetable_SingleBladelink : CompTargetable
+    {
+        protected override TargetingParameters GetTargetingParameters()
+        {
+            return new TargetingParameters
+            {
+                canTargetPawns = false,
+                canTargetBuildings = false,
+                canTargetItems = true,
+                mapObjectTargetsMustBeAutoAttackable = false,
+                validator = x => x.Thing.TryGetComp<CompBladelinkWeapon>() != null && x.Thing.TryGetComp<CompBladelinkWeapon>().CodedPawn != null && BaseTargetValidator(x.Thing)
+            };
+        }
+
+        public override IEnumerable<Thing> GetTargets(Thing targetChosenByPlayer = null)
+        {
+            yield return targetChosenByPlayer;
+        }
+
+        protected override bool PlayerChoosesTarget
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
