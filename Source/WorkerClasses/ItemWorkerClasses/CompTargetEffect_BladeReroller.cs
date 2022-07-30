818c3be05f6819cd48dabf971930218078c2a792
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-07-30 16:25:50 +0200

    refactored CompTargetEffect_BladeReroller to not use item instance as seed

--- Source/WorkerClasses/ItemWorkerClasses/CompTargetEffect_BladeReroller.cs
+++ Source/WorkerClasses/ItemWorkerClasses/CompTargetEffect_BladeReroller.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection;
+using System.Linq;
+using HarmonyLib;
 using RimWorld;
 using Verse;
 
@@ -8,16 +9,61 @@ namespace MorePersonaTraits.WorkerClasses.ItemWorkerClasses
 {
     public class CompTargetEffect_BladeReroller : CompTargetEffect
     {
+        // private static readonly AccessTools.FieldRef<object, List<WeaponTraitDef>> weaponTraitsField =
+        //     AccessTools.FieldRefAccess<List<WeaponTraitDef>>(typeof(CompBladelinkWeapon), "traits");
+
         public override void DoEffectOn(Pawn user, Thing target)
         {
-            var compBladelink = target.TryGetComp<CompBladelinkWeapon>();
-            if (compBladelink == null)
+            try
             {
-                return;
+                CompBladelinkWeapon compBladelink = target.TryGetComp<CompBladelinkWeapon>();
+
+                if (compBladelink == null) return;
+
+                RerollTraits(compBladelink);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+            }
+        }
+
+        private void RerollTraits(CompBladelinkWeapon compBladelink)
+        {
+            List<WeaponTraitDef> traits = AccessTools
+                .FieldRefAccess<List<WeaponTraitDef>>(typeof(CompBladelinkWeapon), "traits")
+                .Invoke(compBladelink);
+
+            traits.Clear();
+            InitializeTraits(traits);
+        }
+
+        private void InitializeTraits(List<WeaponTraitDef> existingTraits)
+        {
+            if (existingTraits == null) existingTraits = new List<WeaponTraitDef>();
+
+            //TODO: Get this from a setting
+            int randomInRange = new IntRange(1, 2).RandomInRange;
+
+            for (var index = 0; index < randomInRange; ++index)
+            {
+                var availableTraits = DefDatabase<WeaponTraitDef>.AllDefs
+                    .Where(possibleTrait => CanAddTrait(existingTraits, possibleTrait))
+                    .ToList();
+
+                if (!availableTraits.NullOrEmpty())
+                    existingTraits.Add(availableTraits.RandomElementByWeight(x => x.commonality));
+            }
+        }
+
+        private bool CanAddTrait(List<WeaponTraitDef> existingTraits, WeaponTraitDef traitToAdd)
+        {
+            if (!existingTraits.NullOrEmpty())
+            {
+                return existingTraits.Exists(existingTrait => traitToAdd.Overlaps(existingTrait));
             }
 
-            compBladelink.TraitsListForReading.Clear();
-            target.TryGetComp<CompBladelinkWeapon>().PostPostMake();
+            return true;
         }
     }
 }
