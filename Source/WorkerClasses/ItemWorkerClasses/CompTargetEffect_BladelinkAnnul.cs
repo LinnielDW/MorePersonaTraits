32e44a2938c94bbc8a1063ac2684c6f3f9df72f8
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-08-01 11:37:31 +0200

    added MPT_BladelinkAnnul and refactored MPT_Bladelink_Items

---
+++ Source/WorkerClasses/ItemWorkerClasses/CompTargetEffect_BladelinkAnnul.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using MorePersonaTraits.Utils;
+using RimWorld;
+using Verse;
+
+namespace MorePersonaTraits.WorkerClasses.ItemWorkerClasses
+{
+    public class CompTargetEffect_BladelinkAnnul : CompTargetEffect
+    {
+        public override void DoEffectOn(Pawn user, Thing target)
+        {
+            var compBladelink = target.TryGetComp<CompBladelinkWeapon>();
+            List<WeaponTraitDef> existingTraits = AccessTools
+                .FieldRefAccess<List<WeaponTraitDef>>(typeof(CompBladelinkWeapon), "traits")
+                .Invoke(compBladelink);
+
+
+            if (compBladelink == null)
+            {
+                return;
+            }
+
+            if (existingTraits.NullOrEmpty())
+            {
+                TraitUtils.InitializeTraits(compBladelink);
+                // Log.Error("[MorePersonaTraits]: No traits to remove on this weapon. This should have been caught by the targeting class. Please let the mod author know.");
+            }
+            else
+            {
+                existingTraits.RemoveAt(Rand.Range(0, existingTraits.Count - 1));
+                if (existingTraits.NullOrEmpty()) TraitUtils.InitializeTraits(compBladelink);
+            }
+        }
+    }
+}
---
+++ Source/WorkerClasses/ItemWorkerClasses/CompTargetable_SingleBladelinkTraitRemovable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using MorePersonaTraits.Utils;
+using RimWorld;
+using Verse;
+
+namespace MorePersonaTraits.WorkerClasses.ItemWorkerClasses
+{
+    public class CompTargetable_SingleBladelinkTraitRemovable : CompTargetable
+    {
+        protected override TargetingParameters GetTargetingParameters()
+        {
+            return new TargetingParameters
+            {
+                canTargetPawns = false,
+                canTargetBuildings = false,
+                canTargetItems = true,
+                mapObjectTargetsMustBeAutoAttackable = false,
+                validator = targetInfo =>
+                    targetInfo.Thing.TryGetComp<CompBladelinkWeapon>() != null && TargetUtils.ValidateRequiresBond(Props, targetInfo) && !targetInfo.Thing.TryGetComp<CompBladelinkWeapon>().TraitsListForReading.NullOrEmpty() &&
+                    BaseTargetValidator(targetInfo.Thing)
+            };
+        }
+
+        public override IEnumerable<Thing> GetTargets(Thing targetChosenByPlayer = null)
+        {
+            yield return targetChosenByPlayer;
+        }
+
+        protected override bool PlayerChoosesTarget => true;
+    }
+}
