3a8337cd91c653558b0eeb3988c30f65a373f4bc
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-08-02 20:35:29 +0200

    added MPT_OnKill_NeedFilledFood

---
+++ Source/WorkerClasses/WeaponTraitWorkerClasses/WeaponTraitWorker_FoodFilledOnKill.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MorePersonaTraits.WorkerClasses.WeaponTraitWorkerClasses
+{
+    public static class NeedFillUtil
+    {
+        public static void FillNeed(NeedDef Need, Pawn pawn)
+        {
+            var x = pawn.needs.TryGetNeed(Need);
+            if (x == null)
+            {
+                return;
+            }
+
+            x.CurLevel = x.MaxLevel;
+        }
+    }
+
+    public class WeaponTraitWorker_FoodFilledOnKill : WeaponTraitWorker
+    {
+        public override void Notify_KilledPawn(Pawn pawn)
+        {
+            base.Notify_KilledPawn(pawn);
+            NeedFillUtil.FillNeed(NeedDefOf.Food, pawn);
+        }
+    }
+}
