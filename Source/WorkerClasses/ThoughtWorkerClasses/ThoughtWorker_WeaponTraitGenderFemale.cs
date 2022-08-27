1a9f526c6c9c25c44ecc5778e9ca5e407339aa8e
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-08-27 19:59:26 +0200

    added MPT_BondedThought Female/male love and hate

---
+++ Source/WorkerClasses/ThoughtWorkerClasses/ThoughtWorker_WeaponTraitGenderFemale.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MorePersonaTraits.WorkerClasses.ThoughtWorkerClasses
+{
+    public class ThoughtWorker_WeaponTraitGenderFemale : ThoughtWorker_WeaponTrait
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            if (!base.CurrentStateInternal(p).Active)
+            {
+                return ThoughtState.Inactive;
+            }
+
+            return
+                p.equipment.bondedWeapon.TryGetComp<CompBladelinkWeapon>().TraitsListForReading.Exists(trait => trait.bondedThought == def)
+                && p.gender == Gender.Female
+                    ? true
+                    : ThoughtState.Inactive;
+        }
+    }
+}
