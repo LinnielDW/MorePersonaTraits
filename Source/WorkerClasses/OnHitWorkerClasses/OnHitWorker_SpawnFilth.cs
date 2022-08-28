93a7f9e5a1644e62bdf9c0205668b076df370d3f
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-08-28 20:27:27 +0200

    added MPT_OnHitSpawnAsh

---
+++ Source/WorkerClasses/OnHitWorkerClasses/OnHitWorker_SpawnFilth.cs
@@ -0,0 +1,21 @@
+﻿using MorePersonaTraits.Utils;
+using RimWorld;
+using Verse;
+
+namespace MorePersonaTraits.WorkerClasses.OnHitWorkerClasses
+{
+    public class OnHitWorker_SpawnFilth : OnHitWorker
+    {
+        public ThingDef Filth = null;
+
+        public override void OnHitEffect(Thing hitThing, Thing originThing)
+        {
+            OnHitEffect(hitThing, originThing, ApplyFilthToPawn);
+        }
+
+        private void ApplyFilthToPawn(Pawn pawn)
+        {
+            OnHitUtils.makeFilth(pawn.Position, pawn.Map, Filth);
+        }
+    }
+}
