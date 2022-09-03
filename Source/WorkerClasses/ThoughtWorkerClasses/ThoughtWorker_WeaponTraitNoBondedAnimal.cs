efcb69707cc1d5eec05c1b76d28fd8a4a2c979cd
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-09-03 14:48:28 +0200

    added thought_animal traits

---
+++ Source/WorkerClasses/ThoughtWorkerClasses/ThoughtWorker_WeaponTraitNoBondedAnimal.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MorePersonaTraits.WorkerClasses.ThoughtWorkerClasses
+{
+    public class ThoughtWorker_WeaponTraitNoBondedAnimal : ThoughtWorker_WeaponTrait
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            if (!base.CurrentStateInternal(p).Active)
+            {
+                return ThoughtState.Inactive;
+            }
+
+            return
+                p.equipment.bondedWeapon.TryGetComp<CompBladelinkWeapon>().TraitsListForReading.Exists(trait => trait.bondedThought == def)
+                && !p.relations.DirectRelations.Exists(relation => relation.def == PawnRelationDefOf.Bond && relation.otherPawn.playerSettings.RespectedMaster == p)
+                    ? true
+                    : ThoughtState.Inactive;
+        }
+        
+    }
+}
