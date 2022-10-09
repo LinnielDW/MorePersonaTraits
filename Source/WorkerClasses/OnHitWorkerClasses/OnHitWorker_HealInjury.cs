227969a17e7341659d13921d211ce961209177dc
Linniel Driver-Williams <LinnielDW@googlemail.com>
2022-10-09 21:37:35 +0200

    added MPT_OnHit_HealWielder

---
+++ Source/WorkerClasses/OnHitWorkerClasses/OnHitWorker_HealInjury.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace MorePersonaTraits.WorkerClasses.OnHitWorkerClasses
+{
+    public class OnHitWorker_HealInjury : OnHitWorker
+    {
+        public override void OnHitEffect(Thing hitThing, Thing originThing)
+        {
+            ApplyOnHitEffect(hitThing, originThing, HealInjury);
+        }
+
+        private void HealInjury(Thing thing)
+        {
+            var injuries = new List<Hediff_Injury>();
+            (thing as Pawn)?.health.hediffSet.GetHediffs(ref injuries, h => h.CanHealNaturally());
+
+            injuries.RandomElement().Heal(ProcMagnitude * ((Pawn) thing).HealthScale);
+        }
+    }
+}
