77780567c5b035fe38e169e547ecef355c36fe60
Linniel Driver-Williams <LinnielDW@googlemail.com>
2023-04-23 14:03:37 +0200

    tidyup

--- Source/MorePersonaTraits.cs
+++ Source/MorePersonaTraits.cs
@@ -4,29 +4,28 @@ using MorePersonaTraits.Settings;
 using UnityEngine;
 using Verse;
 
-namespace MorePersonaTraits
+namespace MorePersonaTraits;
+
+public class MorePersonaTraits : Mod
 {
-    public class MorePersonaTraits : Mod
+    readonly MorePersonaTraitsSettings settings;
+
+    public MorePersonaTraits(ModContentPack content) : base(content)
     {
-        readonly MorePersonaTraitsSettings settings;
+        settings = GetSettings<MorePersonaTraitsSettings>();
 
-        public MorePersonaTraits(ModContentPack content) : base(content)
-        {
-            settings = GetSettings<MorePersonaTraitsSettings>();
+        var harmony = new Harmony("com.arquebus.rimworld.mod.morepersonatraits");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
 
-            var harmony = new Harmony("com.arquebus.rimworld.mod.morepersonatraits");
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
-        }
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        settings.DoSettingsWindowContents(inRect);
+        base.DoSettingsWindowContents(inRect);
+    }
 
-        public override void DoSettingsWindowContents(Rect inRect)
-        {
-            settings.DoSettingsWindowContents(inRect);
-            base.DoSettingsWindowContents(inRect);
-        }
-
-        public override string SettingsCategory()
-        {
-            return "MPT_SettingsTitle".Translate();
-        }
+    public override string SettingsCategory()
+    {
+        return "MPT_SettingsTitle".Translate();
     }
 }
