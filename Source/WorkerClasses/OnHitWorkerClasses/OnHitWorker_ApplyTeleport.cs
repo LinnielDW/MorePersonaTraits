2c6b42f5818a7e59afe0db889b9f05fc497ad1ea
Linniel Driver-Williams <LinnielDW@googlemail.com>
2023-09-11 17:27:33 +0200

    added OnHitWorker_ApplyTeleport

---
+++ Source/WorkerClasses/OnHitWorkerClasses/OnHitWorker_ApplyTeleport.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace MorePersonaTraits.WorkerClasses.OnHitWorkerClasses;
+
+public class OnHitWorker_ApplyTeleport : OnHitWorker
+{
+    public FloatRange RandomRange;
+    public IntRange stunTicks;
+    private List<IntVec3> cells = new List<IntVec3>();
+
+    public override void OnHitEffect(Thing hitThing, Thing originThing)
+    {
+        ApplyOnHitEffect(hitThing, originThing, delegate(Thing targetThing) { ApplyTeleportToPawn(originThing, targetThing); });
+    }
+
+    private void ApplyTeleportToPawn(Thing originThing, Thing targetThing)
+    {
+        cells.Clear();
+        foreach (var intVec2 in GenRadial.RadialCellsAround(targetThing.Position, RandomRange.RandomInRange, true))
+        {
+            if (intVec2.Standable(targetThing.Map) && GenSight.LineOfSight(targetThing.Position, intVec2, targetThing.Map))
+            {
+                cells.Add(intVec2);
+            }
+        }
+
+        var targetPawn = targetThing as Pawn;
+        if (cells.Any() && targetPawn != null)
+        {
+            EffecterDefOf.Skip_EntryNoDelay.Spawn(targetPawn, targetPawn.Map, 1f);
+            targetPawn.Position = cells.RandomElement();
+
+            targetPawn.stances.stunner.StunFor(stunTicks.RandomInRange, originThing, false, false);
+            targetPawn.Notify_Teleported(true, true);
+            CompAbilityEffect_Teleport.SendSkipUsedSignal(targetPawn.Position, targetPawn);
+
+            EffecterDefOf.Skip_Exit.Spawn(targetPawn, targetPawn.Map, 1f);
+        }
+    }
+}
