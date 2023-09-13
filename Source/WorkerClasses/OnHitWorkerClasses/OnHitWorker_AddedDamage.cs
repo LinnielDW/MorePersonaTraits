8e7ceb8e167a57fd76df9442c009e74ad2e7f0b4
Linniel Driver-Williams <LinnielDW@googlemail.com>
2023-09-13 12:51:08 +0200

    implemented OnHitWorker_AddedDamage

--- Source/WorkerClasses/OnHitWorkerClasses/OnHitWorker_AddedDamage.cs
+++ Source/WorkerClasses/OnHitWorkerClasses/OnHitWorker_AddedDamage.cs
@@ -1,18 +1,39 @@
-﻿using System;
-using Verse;
+﻿using Verse;
 
 namespace MorePersonaTraits.WorkerClasses.OnHitWorkerClasses
 {
     public class OnHitWorker_AddedDamage : OnHitWorker
     {
+        public DamageDef DamageDef = null;
+
         public override void OnHitEffect(Thing hitThing, Thing originThing)
         {
-            ApplyOnHitEffect(hitThing, originThing, ApplyExtraDamage);
+            ApplyOnHitEffect(hitThing, originThing, delegate(Thing thing) { ApplyExtraDamage(originThing, thing); });
         }
 
-        private void ApplyExtraDamage(Thing targetThing)
+        private void ApplyExtraDamage(Thing originThing, Thing targetThing)
         {
-            throw new NotImplementedException();
-        } 
+            if (targetThing == null || targetThing.Destroyed || originThing is not Pawn originPawn)
+            {
+                return;
+            }
+
+            var originVerb = originPawn.equipment?.PrimaryEq.PrimaryVerb;
+            if (originVerb == null)
+            {
+                return;
+            }
+
+            var damageInfo = new DamageInfo(DamageDef, ProcMagnitude, originVerb.verbProps.AdjustedArmorPenetration(originVerb, originPawn), -1f, originPawn, null, originPawn.equipment.Primary.def);
+            damageInfo.SetBodyRegion(BodyPartHeight.Undefined, BodyPartDepth.Outside);
+            damageInfo.SetWeaponBodyPartGroup(originVerb.verbProps.AdjustedLinkedBodyPartsGroup(originVerb.tool));
+            if (originVerb.HediffCompSource != null)
+            {
+                damageInfo.SetWeaponHediff(originVerb.HediffCompSource.Def);
+            }
+
+            damageInfo.SetAngle((targetThing.Position - originPawn.Position).ToVector3());
+            targetThing.TakeDamage(damageInfo);
+        }
     }
 }
