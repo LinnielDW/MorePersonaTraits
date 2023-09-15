2620977ea3e1c176838be72fa844d0245f733893
Linniel Driver-Williams <LinnielDW@googlemail.com>
2023-09-15 11:32:50 +0200

    added WeaponTraitOnKillExtension

---
+++ Source/Extensions/WeaponTraitOnKillExtension.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+
+namespace MorePersonaTraits.Extensions
+{
+    public class WeaponTraitOnKillExtension : DefModExtension
+    {
+        
+        public static WeaponTraitOnKillExtension CreateInstance()
+        {
+            return new WeaponTraitOnKillExtension();
+        }
+
+        //Weapon trait workers don't have access to the target while doing on-kill effects, so instead we add a hediff to the target, and have the hediff do what we want
+        public HediffDef HediffDef = null;
+    }
+}
---
+++ Source/Patches/Pawn_Kill_Patches.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using HarmonyLib;
+using MorePersonaTraits.Extensions;
+using RimWorld;
+using Verse;
+
+namespace MorePersonaTraits.Patches;
+
+[HarmonyPatch(typeof(Pawn))]
+[HarmonyPatch("Kill")]
+public static class Pawn_Kill_Patches
+{
+    static void Prefix(Pawn __instance, DamageInfo? dinfo)
+    {
+        if (!__instance.Spawned || __instance.Destroyed || __instance.Dead)
+        {
+            //Log.Warning("Already dead, skipping");
+            return;
+        }
+
+        var primary = (dinfo?.Instigator as Pawn)?.equipment.Primary;
+        if (primary == null || dinfo.Value.Weapon != primary.def)
+        {
+            return;
+        }
+
+        foreach (var hediffDef in primary.TryGetComp<CompBladelinkWeapon>().TraitsListForReading.Select(trait => trait.GetModExtension<WeaponTraitOnKillExtension>()?.HediffDef).Where(hediffDef => hediffDef != null))
+        {
+            var hediff = HediffMaker.MakeHediff(hediffDef, __instance);
+            __instance.health?.hediffSet.AddDirect(hediff);
+        }
+    }
+}
