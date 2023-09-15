a5e1c0867d65b2e09004d73aef82c3e9ef543a88
Linniel Driver-Williams <LinnielDW@googlemail.com>
2023-09-15 16:35:16 +0200

    recompile

---
+++ Source/WorkerClasses/WeaponTraitWorkerClasses/WeaponTraitWorker_GambaOnEquip.cs
@@ -0,0 +1,9 @@
+﻿// using RimWorld;
+//
+// namespace MorePersonaTraits.WorkerClasses.WeaponTraitWorkerClasses;
+//
+//TODO: IMPL
+// public class WeaponTraitWorker_GambaOnEquip : WeaponTraitWorker
+// {
+//     
+// }
