c2b1e0bd848bc5385c6c20531bc263ee131ac701
Linniel Driver-Williams <LinnielDW@googlemail.com>
2023-10-21 20:23:07 +0200

    styling

--- Source/Patches/BladeWhispererPatches.cs
+++ Source/Patches/BladeWhispererPatches.cs
@@ -15,32 +15,32 @@ public static class BladeWhisperer_Notify_Equipped_Patch
 {
     static void Postfix(Pawn pawn, ThingWithComps __instance)
     {
-        if (pawn != null && pawn.story?.traits.HasTrait(MPT_WeaponTraitDefOf.MPT_BladeWhisperer) == true && pawn.equipment.bondedWeapon == null && __instance.def.IsMeleeWeapon)
+        if (pawn == null || pawn.story?.traits.HasTrait(MPT_WeaponTraitDefOf.MPT_BladeWhisperer) != true || pawn.equipment.bondedWeapon != null || !__instance.def.IsMeleeWeapon)
         {
-            if (__instance.def.HasComp(typeof(CompBladelinkWeapon)) || __instance.TryGetComp<CompBladelinkWeapon>() != null)
-            {
-                return;
-            }
+            return;
+        }
+        if (__instance.def.HasComp(typeof(CompBladelinkWeapon)) || __instance.TryGetComp<CompBladelinkWeapon>() != null)
+        {
+            return;
+        }
+        if (__instance.def.HasComp(typeof(CompBiocodable)) && __instance.TryGetComp<CompBiocodable>()?.Biocoded == true)
+        {
+            return;
+        }
 
-            if (__instance.def.HasComp(typeof(CompBiocodable)) && __instance.TryGetComp<CompBiocodable>()?.Biocoded == true)
-            {
-                return;
-            }
-
-            CompBladelinkWeapon thingComp = new CompBladelinkWeapon();
-            try
-            {
-                thingComp.parent = __instance;
-                //TODO: make this only initialize 1 trait instead (update the description too)
-                AccessTools.Method("CompBladelinkWeapon:InitializeTraits").Invoke(thingComp, null);
-                __instance.AllComps.Add(thingComp);
-                thingComp.CodeFor(pawn);
-            }
-            catch (Exception ex)
-            {
-                Log.Error("Could not instantiate or initialize a bladelink: " + ex);
-                __instance.AllComps.Remove(thingComp);
-            }
+        CompBladelinkWeapon thingComp = new CompBladelinkWeapon();
+        try
+        {
+            thingComp.parent = __instance;
+            //TODO: make this only initialize 1 trait instead (update the description too)
+            AccessTools.Method("CompBladelinkWeapon:InitializeTraits").Invoke(thingComp, null);
+            __instance.AllComps.Add(thingComp);
+            thingComp.CodeFor(pawn);
+        }
+        catch (Exception ex)
+        {
+            Log.Error("Could not instantiate or initialize a bladelink: " + ex);
+            __instance.AllComps.Remove(thingComp);
         }
     }
 }
