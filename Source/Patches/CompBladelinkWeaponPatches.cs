6d75c2a03d21ff503d478c4bb756b0cb62527d23
Linniel Driver-Williams <LinnielDW@googlemail.com>
2024-04-30 19:03:50 +0200

    added documentation comment

--- Source/Patches/CompBladelinkWeaponPatches.cs
+++ Source/Patches/CompBladelinkWeaponPatches.cs
@@ -12,6 +12,7 @@ namespace MorePersonaTraits.Patches
     {
         static bool Prefix(WeaponTraitDef trait, ref bool __result)
         {
+            //if trait is allowed to spawn
             if (WeaponTraitSpawnSettings.TryGetValue(trait.defName, true)) return true;
 
             __result = false;
