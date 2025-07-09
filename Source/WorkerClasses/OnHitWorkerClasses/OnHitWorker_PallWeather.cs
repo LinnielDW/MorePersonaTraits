02f51aafc696966d77cf915902466ecc141ba0b0
Linniel Driver-Williams <LinnielDW@googlemail.com>
2025-07-09 20:58:05 +0200

    fixed SCOS of new weather worker

--- Source/WorkerClasses/OnHitWorkerClasses/OnHitWorker_PallWeather.cs
+++ Source/WorkerClasses/OnHitWorkerClasses/OnHitWorker_PallWeather.cs
@@ -4,10 +4,15 @@ using Verse;
 
 namespace MorePersonaTraits.WorkerClasses.OnHitWorkerClasses;
 
+[StaticConstructorOnStartup]
+public static class PallCollection
+{
+    public static readonly GameConditionDef[] deathWeathers = { GameConditionDefOf.GrayPall, GameConditionDefOf.DeathPall };
+}
+
 public class OnHitWorker_PallWeather : OnHitWorker
 {
     private FloatRange GrayPallConditionDaysRange = new(1f, 3f);
-    private GameConditionDef[] deathWeathers = { GameConditionDefOf.GrayPall, GameConditionDefOf.DeathPall };
 
     public override void OnHitEffect(Thing hitThing, Thing originThing)
     {
@@ -19,7 +24,7 @@ public class OnHitWorker_PallWeather : OnHitWorker
         if (thing.Map.weatherManager.curWeatherAge < 1600 || thing.Map.weatherDecider.ForcedWeather != null) return;
             
         var num = Mathf.CeilToInt(GrayPallConditionDaysRange.RandomInRange * 60000f);
-        var chosenCondition = Rand.Element(deathWeathers);
+        var chosenCondition = Rand.Element(PallCollection.deathWeathers);
             
         GameConditionMaker.MakeCondition(chosenCondition, num);
 
